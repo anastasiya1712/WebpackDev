53d9f3ad15f35133f9e12063cdb2416af0f5e631
AnastasiyaPerepechkina <perepechkina.anastasiya.2002@yandex.ru>
2025-07-01 16:55:30 -0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-const numbers:number[] = [2, 3, 5];
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
+import style from './index.module.scss';
+import './index.scss';
 
-console.log(doubledNumbers); // 4, 6, 10
+console.log(style); //использование объекта style важно для попадания стилей в css-бандл
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "style-loader",
+          //"style-loader",
           {
             loader: "css-loader",
             options: {
